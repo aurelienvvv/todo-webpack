8773c51b40a1e58eae0615c89b004c41e28499d0
Aurélien Verschueren <averschueren@modedemploi.fr>
2021-03-10 18:52:19 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './js/helpers.js';
+import './js/store.js';
+import './js/model.js';
+import './js/template.js';
+import './js/view.js';
+import './js/controller.js';
+import './js/app.js';
+import './styles/main.scss';
+
+// tous les fichiers JS appelés ici
+
+console.log(bro('Dude'));
